e5437b5be8d9148f905efea67e468c3adc3c4617
anerie <141164361+anerie-anerie@users.noreply.github.com>
2024-12-22 17:41:49 -0500

    Update ultrasonicPlate.java

--- src/main/java/frc/robot/subsystems/ultrasonicPlate.java
+++ src/main/java/frc/robot/subsystems/ultrasonicPlate.java
@@ -1,7 +1,14 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ultrasonicPlate extends SubsystemBase{
-    
+    Ultrasonic m_rangeFinder = new Ultrasonic(7, 6);
+    double distanceMillimeters = m_rangeFinder.getRangeMM();
+    private final double threashholdMillimeters = 5.0e1;
+      
+    public boolean onPlate() {
+        return (distanceMillimeters < threashholdMillimeters);
+    }
 }
