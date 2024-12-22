2f7bbab235020e279fe410d7b8582cff31d80867
anerie <141164361+anerie-anerie@users.noreply.github.com>
2024-12-22 17:38:28 -0500

    Update ElevatorUp.java

--- src/main/java/frc/robot/commands/ElevatorUp.java
+++ src/main/java/frc/robot/commands/ElevatorUp.java
@@ -1,7 +1,28 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.KrakenMotorSubsystem;
 
-public class ElevatorUp extends Command{
-    
+public class ElevatorUp extends CommandBase {
+    private final KrakenMotorSubsystem elevator;
+
+    public ElevatorUp(KrakenMotorSubsystem elevator) {
+        this.elevator = elevator;
+        addRequirements(elevator);
+    }
+
+    @Override
+    public void execute() {
+        elevator.move(frc.robot.Constants.ElevatorConstants.ELEVATOR_SPEED);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        elevator.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false; // Runs until interrupted
+    }
 }
