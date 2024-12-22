71c39e2c3351934224ec5dc1b3b288aaf0be1d97
anerie <141164361+anerie-anerie@users.noreply.github.com>
2024-12-22 17:38:40 -0500

    Update ElevatorDown.java

--- src/main/java/frc/robot/commands/ElevatorDown.java
+++ src/main/java/frc/robot/commands/ElevatorDown.java
@@ -1,7 +1,28 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.KrakenMotorSubsystem;
 
-public class ElevatorDown extends Command{
-    
+public class ElevatorDown extends CommandBase {
+    private final KrakenMotorSubsystem elevator;
+
+    public ElevatorDown(KrakenMotorSubsystem elevator) {
+        this.elevator = elevator;
+        addRequirements(elevator);
+    }
+
+    @Override
+    public void execute() {
+        elevator.move(-frc.robot.Constants.ElevatorConstants.ELEVATOR_SPEED);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        elevator.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false; // Runs until interrupted
+    }
 }
