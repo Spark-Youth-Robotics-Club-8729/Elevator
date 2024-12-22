d37f20aa3508d0673d891f15ea80ed30b32abfe2
anerie <141164361+anerie-anerie@users.noreply.github.com>
2024-12-22 17:41:05 -0500

    Update ultrasonicGround.java

--- src/main/java/frc/robot/subsystems/ultrasonicGround.java
+++ src/main/java/frc/robot/subsystems/ultrasonicGround.java
@@ -1,7 +1,15 @@
+
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ultrasonicGround extends SubsystemBase{
-    
+    Ultrasonic m_rangeFinder = new Ultrasonic(11, 10);
+    double distanceMillimeters = m_rangeFinder.getRangeMM();
+    private final double threashholdMillimeters = 2.0e1;
+      
+    public boolean onGround() {
+        return (distanceMillimeters < threashholdMillimeters);
+    }
 }
