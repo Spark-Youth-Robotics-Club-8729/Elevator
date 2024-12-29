3569578308336850cf348a6846d3f9d9edfe0bf7
anerie <141164361+anerie-anerie@users.noreply.github.com>
2024-12-29 10:49:33 -0500

    Update KrakenMotorSubsystem.java

--- src/main/java/frc/robot/subsystems/KrakenMotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/KrakenMotorSubsystem.java
@@ -1,33 +1,41 @@
+
 package frc.robot.subsystems;
 
+import frc.robot.Constants.MotorConstants;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.MathUtil;
+// import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 //import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 //import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix6.hardware.TalonFX;
 // import com.ctre.phoenix6.configs.TalonFXConfiguration;
 // import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.revrobotics.RelativeEncoder;
 
 public class KrakenMotorSubsystem extends SubsystemBase {
-
   // Motor controller for Kraken motor
-  private final TalonFX krakenMotor;
+  private final TalonFX krakenMotor = new TalonFX(MotorConstants.KRAKEN_MOTOR_CAN_ID);
 
-  // Constructor: Initialize the Kraken motor with TalonFX
-  public KrakenMotorSubsystem(int motorPort) {
-    // Initialize the TalonFX motor controller with the given motor port
-    this.krakenMotor = new TalonFX(motorPort); // Use TalonFX to control the Kraken X60 motor
+  public KrakenMotorSubsystem () {
+    
   }
 
+  public double getRotationEncoder() {
+    double currentValue = krakenMotor.getPosition().getValue();
+    return currentValue;
+}
   // Method to set motor speed
   public void setMotorSpeed(double speed) {
     // The speed can range from -1.0 to 1.0 (full reverse to full forward)
-    krakenMotor.set(speed);
+    krakenMotor.setRotate(speed);
   }
 
   // Method to stop the motor (set speed to 0)
   public void stopMotor() {
-    krakenMotor.set(0);
+    krakenMotor.setRotate(0);
   }
 
   // Periodic method for subsystem updates
@@ -35,5 +43,7 @@ public class KrakenMotorSubsystem extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
     // Add any monitoring or logging logic here if necessary
+    // for this version using the encoder won't be nesseary but as we add more to it we will need to use it have presets
+    SmartDashboard.putNumber("Rotation Encoder", getRotationEncoder());
   }
 }
