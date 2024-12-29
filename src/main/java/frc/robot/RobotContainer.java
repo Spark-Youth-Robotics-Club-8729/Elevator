e664029bbf4cece3b768004a68859b87209e260e
anerie <141164361+anerie-anerie@users.noreply.github.com>
2024-12-29 10:13:03 -0500

    updated with corrected commands for the joystick and initalization

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,24 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.JoystickConstants;
 import frc.robot.commands.ElevatorUp;
 import frc.robot.commands.ElevatorDown;
 import frc.robot.subsystems.KrakenMotorSubsystem;
 
-public class RobotContainer {
-    private final KrakenMotorSubsystem elevator = new KrakenMotorSubsystem(0);
-    private final Joystick joystick = new Joystick(frc.robot.Constants.JoystickConstants.JOYSTICK_PORT);
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+    // The robot's subsystems and commands are defined here...
+    private final KrakenMotorSubsystem elevatorSubsystem = new KrakenMotorSubsystem();
+    private final CommandXboxController operatorJoystick = new CommandXboxController(JoystickConstants.JOYSTICK_PORT);
+
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
     public RobotContainer() {
-        configureButtonBindings();
+        // Configure the button bindings
+        configureBindings();
     }
 
-    private void configureButtonBindings() {
-        new JoystickButton(joystick, frc.robot.Constants.JoystickConstants.UP_BUTTON)
-            .whileTrue(new ElevatorUp(elevator));
+    /* Triggering commands with these buttons being pressed */
+    private void configureBindings() {
+        operatorJoystick.b().onTrue(new ElevatorUp(elevatorSubsystem));
+        operatorJoystick.a().onTrue(new ElevatorDown(elevatorSubsystem));
+        
 
-        new JoystickButton(joystick, frc.robot.Constants.JoystickConstants.DOWN_BUTTON)
-            .whileTrue(new ElevatorDown(elevator));
     }
 }
