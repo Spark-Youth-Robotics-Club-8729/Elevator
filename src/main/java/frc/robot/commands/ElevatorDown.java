5fc5433fa3991bed46a986afc81c239dfa805053
anerie <141164361+anerie-anerie@users.noreply.github.com>
2024-12-29 10:49:08 -0500

    Update ElevatorDown.java

--- src/main/java/frc/robot/commands/ElevatorDown.java
+++ src/main/java/frc/robot/commands/ElevatorDown.java
@@ -1,21 +1,26 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.KrakenMotorSubsystem;
 
-public class ElevatorDown extends Command {
+public class ElevatorDown extends CommandBase {
     private final KrakenMotorSubsystem elevator;
+    private final double speed;
 
-    public ElevatorDown(KrakenMotorSubsystem elevator) {
+
+    public ElevatorDown(KrakenMotorSubsystem elevator, double speed) {
         this.elevator = elevator;
+        this.speed = speed;
         addRequirements(elevator);
     }
 
+    // when button pressed rotate the opposite way
     @Override
     public void execute() {
-        elevator.setMotorSpeed(-frc.robot.Constants.ElevatorConstants.ELEVATOR_SPEED);
+        elevator.setMotorSpeed(-speed);
     }
 
+    // when button not pressed stop rotating
     @Override
     public void end(boolean interrupted) {
         elevator.stopMotor();
