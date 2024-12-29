91b36beee6801dc2073dec4aeaa51f0530b240fc
anerie <141164361+anerie-anerie@users.noreply.github.com>
2024-12-29 10:13:31 -0500

    corrected with only needed constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,8 +6,7 @@ public final class Constants {
     // constants for the joystick
     public static final class JoystickConstants {
         public static final int JOYSTICK_PORT = 0;
-        public static final int UP_BUTTON = 1;
-        public static final int DOWN_BUTTON = 2;
+
     }
 
     // constants for the kraken
